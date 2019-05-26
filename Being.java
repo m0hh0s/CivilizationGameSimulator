7f22bde8719c94df03d49563ba6bbba31e3e9f29
9731016 <9731016@ceit.local>
2019-05-26 19:23:13 +0430

    Being class modified

--- Being.java
+++ Being.java
@@ -1,2 +1,31 @@
-public class Being {
+public abstract class Being {
+    private Location location;
+    private Weapon weapon;
+    private Armor armor;
+    private boolean isAlive;
+
+    public Being(Location location, Weapon weapon, Armor armor, boolean isAlive) {
+        this.location = location;
+        this.weapon = weapon;
+        this.armor = armor;
+        this.isAlive = isAlive;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public Armor getArmor() {
+        return armor;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public abstract void act();
 }

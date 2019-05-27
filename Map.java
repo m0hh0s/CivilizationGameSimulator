bc2fe7ec07044f25106cd82f411454e368e56570
9731016 <9731016@ceit.local>
2019-05-27 14:00:57 +0430

    map class created

---
+++ Map.java
@@ -0,0 +1,21 @@
+public class Map {
+    private static int size;
+    private static Being[][] map;
+
+    public Map(int size) {
+        this.size = size;
+        map = new Being[size][size];
+    }
+
+    public static Being[][] getMap() {
+        return map;
+    }
+
+    public static void setMap(Being[][] map1){
+        map = map1;
+    }
+
+    public static int getSie(){
+        return size;
+    }
+}
